d9dc6c0242ee35838fa913b0ecfb28834e3dc23b
Thuy-Vy Nghiem <thuyvynghiem@yahoo.com>
2025-11-15 20:10:37 +0100

    player move animation

--- Assets/Resources_Meghan/Scripts/PlayerMovement.cs
+++ Assets/Resources_Meghan/Scripts/PlayerMovement.cs
@@ -10,6 +10,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
 
     private Vector2 moveInput;
+    
+    //comment as long as we dont have animation sprites
+    //[Header("Animator Variables")]
+    //[SerializeField] private Animator animator;
+    //[SerializeField] private SpriteRenderer spriteRenderer;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,5 +37,20 @@ public class PlayerMovement : MonoBehaviour
             float angle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
+        
+        //float moveX = moveInput.x;
+        //float moveY = moveInput.y;
+        //animator.SetFloat("MoveX", moveX);
+        //animator.SetFloat("MoveY", moveY);
+        
+        ///flipping, cus we only have left facing sprites
+        //if (moveX < 0)
+        //{
+        //    spriteRenderer.flipX = false;
+        //}
+        //else if (moveX > 0)
+        //{
+        //    spriteRenderer.flipX = true;
+        //}
     }
 }
