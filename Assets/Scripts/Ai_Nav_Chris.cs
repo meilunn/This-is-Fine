1477582152390cc74c3aed5bf7d29279a18f1c42
zuzinami <52863041+zuzinami@users.noreply.github.com>
2025-11-15 12:35:42 +0100

    more textures and moving ai
    rotates through all passanger but doesnt prioritize closest

--- Assets/Scripts/Ai_Nav_Chris.cs
+++ Assets/Scripts/Ai_Nav_Chris.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Ai_Nav_Chris : MonoBehaviour
 {
+    public Transform[] patrolToNPC;
+    private int currentNPCIndex = 0;
+    private NavMeshAgent agent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis =false;
+
+        agent.SetDestination(patrolToNPC[currentNPCIndex].position);
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!agent.pathPending && agent.remainingDistance < 0.1f)
+        {
+
+            currentNPCIndex = (currentNPCIndex + 1) % patrolToNPC.Length;
+            agent.SetDestination(patrolToNPC[currentNPCIndex].position);
+
+        }
         
     }
 }
