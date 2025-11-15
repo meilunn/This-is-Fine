501a2d4db1c6ba3cffd9b0faa84116b485e5b21c
Meihuan Wang <meghanwang22@yahoo.com>
2025-11-15 23:00:12 +0100

    Turns interactionDetector instead of player

--- Assets/Resources_Meghan/Scripts/PlayerMovement.cs
+++ Assets/Resources_Meghan/Scripts/PlayerMovement.cs
@@ -10,12 +10,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rb;
 
+    [SerializeField]
+    private GameObject interactionDetector;
+
     private Vector2 moveInput;
 
     [Header("Animator Variables")]
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer spriteRenderer;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,12 +34,18 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-        
+
+        if (moveInput != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
+            interactionDetector.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        }
+
         float moveX = moveInput.x;
         float moveY = moveInput.y;
         animator.SetFloat("MoveX", moveX);
         animator.SetFloat("MoveY", moveY);
-        
+
         ///flipping, cus we only have left facing sprites
         if (Mathf.Abs(moveX) > Mathf.Abs(moveY) && Mathf.Abs(moveX) > 0.1f)
         {
