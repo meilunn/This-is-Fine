1279a1d24dd12330d50374eb2dd64c6d6bbc0b17
olipoli1233 <ge87juf@mytum.de>
2025-11-15 17:36:16 +0100

    Chasing player fixed

--- Assets/PlayerDetection.cs
+++ Assets/PlayerDetection.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class PlayerDetection : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private TicketControllerAI controller;
+    public void Awake()
     {
-        
-    }
+        // This script lives on a child of the controller
+        controller = GetComponentInParent<TicketControllerAI>();
+        if (controller == null)
+        {
+            Debug.LogError("PlayerDetection: No TicketControllerAI found in parent!");
+        }
+        else
+        {
+            Debug.Log($"PlayerDetection on {name}: controller = {controller.name}");
+        }
+    } 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"PlayerDetection on {name}: OnTriggerEnter2D with {other.name}");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                // Option 1: detect by PlayerMovement (component on the player root)
+        var playerMovement = other.GetComponentInParent<PlayerMovement>();
+
+
+        if (playerMovement != null)
+        {
+            Debug.Log($"{name}: Player detected, telling controller to chase.");
+            controller?.OnPlayerDetected();
+        }
     }
 }
